ddc7456ff2f373d1a4e48950aff55e34b7ea2965
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-15 17:32:49 -0500

    add About info

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ProfileInfo from "./components/ProfileInfo"
+import About from "./components/About"
 import "./sass/main.scss"
 
 function App() {
   return (
     <>
       <ProfileInfo />
+      <About />
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+        <div>
+            <div>
+                <h2>About</h2>
+                <p></p>
+            </div>
+            <div>
+                <h2>Interests</h2>
+                <p></p>
+            </div>
+        </div>
+    )
+}
