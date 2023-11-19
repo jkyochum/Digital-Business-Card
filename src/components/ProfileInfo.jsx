aafef5f2eb50f80364038ed6ba2a4b83ed725d6a
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-19 16:07:15 -0500

    change buttons to divs for better styling

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -6,8 +6,8 @@ export default function ProfileInfo() {
             <h3 className="title">Front-end Developer</h3>
             <p className="website-link"><a href="https://jkyochum.github.io/" target="_blank">jkyochum.github.io</a></p>
             <div className="profile-buttons">
-                <button className="email"><img src="./src/assets/mail-icon.svg" />Email</button>
-                <button className="linkedin"><img src="/src/assets/linkedin-icon.svg" />LinkedIn</button>
+                <div className="email"><img src="./src/assets/mail-icon.svg" />Email</div>
+                <div className="linkedin"><img src="/src/assets/linkedin-icon.svg" />LinkedIn</div>
             </div>
         </header>
     )
