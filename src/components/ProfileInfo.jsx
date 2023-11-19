c947f0274d8a96044de890f8a68093d75df49708
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-19 15:42:22 -0500

    add classNames to profile info component / create page styles

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,10 +1,10 @@
 export default function ProfileInfo() {
     return (
         <header className="profile-info">
-            <img src="./src/assets/jy-logo.svg" alt="My logo" />
-            <h1>Jeremy Yochum</h1>
-            <h3>Front-end Developer</h3>
-            <p><a href="https://jkyochum.github.io/" target="_blank">jkyochum.github.io</a></p>
+            <img src="./src/assets/jy-logo.svg" alt="My logo" className="header-logo" />
+            <h1 className="name">Jeremy Yochum</h1>
+            <h3 className="title">Front-end Developer</h3>
+            <p className="website-link"><a href="https://jkyochum.github.io/" target="_blank">jkyochum.github.io</a></p>
             <div className="profile-buttons">
                 <button className="email"><img src="./src/assets/mail-icon.svg" />Email</button>
                 <button className="linkedin"><img src="/src/assets/linkedin-icon.svg" />LinkedIn</button>
