8fbf6d06ce7a4f246d3cf07c5ff88b0090c6cd70
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-19 13:08:59 -0500

    Add footer component to App.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div>
+            <img src="./src/assets/jy-logo.svg" alt="My personal logo" />
+            <img src="./src/assets/facebook-icon.svg" alt="Facebook logo" />
+            <img src="./src/assets/github-icon.svg" alt="GitHub logo" />
+        </div>
+    )
+}
