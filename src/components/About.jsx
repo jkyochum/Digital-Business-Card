317e8946fe44e61637649b4ff69649e88da268c0
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-19 13:26:17 -0500

    add classnames to about and interests sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
 export default function About() {
     return (
         <div>
-            <div>
+            <div className="about">
                 <h2>About</h2>
                 <p>I am a front-end web developer, husband, and father. I am captivated by web development and enjoy creating beautiful webpages that are smooth and simple for anyone who uses them. I am always on the hunt for a new learning experience, and I know there will never be a lack of opportunity in this ever-changing world of digital art.</p>
             </div>
-            <div>
+            <div className="interests">
                 <h2>Interests</h2>
                 <p>Disc golf. Long hikes. Great food. Live shows. Date nights. Off-roading. Cyberpunk. Sunshine.</p>
             </div>
