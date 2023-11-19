af59aa7be27ae173671420a9759b3db36bb4d2c4
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-19 13:10:31 -0500

    add content to about and interest sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,11 @@ export default function About() {
         <div>
             <div>
                 <h2>About</h2>
-                <p></p>
+                <p>I am a front-end web developer, husband, and father. I am captivated by web development and enjoy creating beautiful webpages that are smooth and simple for anyone who uses them. I am always on the hunt for a new learning experience, and I know there will never be a lack of opportunity in this ever-changing world of digital art.</p>
             </div>
             <div>
                 <h2>Interests</h2>
-                <p></p>
+                <p>Disc golf. Long hikes. Great food. Live shows. Date nights. Off-roading. Cyberpunk. Sunshine</p>
             </div>
         </div>
     )
