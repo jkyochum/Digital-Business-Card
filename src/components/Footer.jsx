dc5db0c7571c532c51a656b8cc0a999b7e93a730
Jeremy Yochum <jyochum391@yahoo.com>
2023-11-19 20:32:52 -0500

    finishing touches

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
         <footer className="footer">
-            <a href="https://jkyochum.github.io/" target="_blank"><img src="/src/images/jy-logo.svg" alt="My personal logo" /></a>
-            <a href="https://www.facebook.com/jeremy.kyleyochum/" target="_blank"><img src="/src/images/facebook-icon.svg" alt="Facebook logo" /></a>
-            <a href="https://github.com/jkyochum" target="_blank"><img src="/src/images/github-icon.svg" alt="GitHub logo" href="https://github.com/jkyochum" /></a>
+            <a href="https://jkyochum.github.io/" target="_blank"><img src="/images/jy-logo.svg" alt="My personal logo" /></a>
+            <a href="https://www.facebook.com/jeremy.kyleyochum/" target="_blank"><img src="/images/facebook-icon.svg" alt="Facebook logo" /></a>
+            <a href="https://github.com/jkyochum" target="_blank"><img src="/images/github-icon.svg" alt="GitHub logo" href="https://github.com/jkyochum" /></a>
         </footer>
     )
 }
--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,13 +1,13 @@
 export default function ProfileInfo() {
     return (
         <header className="profile-info">
-            <img src="/src/images/jy-logo.svg" alt="My logo" className="header-logo" />
+            <img src="/images/jy-logo.svg" alt="My logo" className="header-logo" />
             <h1 className="name">Jeremy Yochum</h1>
             <h3 className="title">Front-end Developer</h3>
             <p className="website-link"><a href="https://jkyochum.github.io/" target="_blank">jkyochum.github.io</a></p>
             <div className="profile-buttons">
-                <a href="mailto:whiterwidow@gmail.com?subject=Reaching Out Via DBC"><div className="email"><img src="/src/images/mail-icon.svg" />Email</div></a>
-                <a href="https://www.linkedin.com/in/jeremy-yochum/" target="_blank"><div className="linkedin"><img src="/src/images/linkedin-icon.svg" />LinkedIn</div></a>
+                <a href="mailto:whiterwidow@gmail.com?subject=Reaching Out Via DBC"><div className="email"><img src="/images/mail-icon.svg" />Email</div></a>
+                <a href="https://www.linkedin.com/in/jeremy-yochum/" target="_blank"><div className="linkedin"><img src="/images/linkedin-icon.svg" />LinkedIn</div></a>
             </div>
         </header>
     )
